fbfde43e76123df22d65049ea9a15042eda80da7
Brandon Minh <229652@shakopeeschools.org>
2019-01-23 14:06:58 -0600

    change code/strategy
    change my strategy and redo the code

--- team11.py
+++ team11.py
@@ -7,14 +7,21 @@
 ####
 
 team_name = 'Brandon Minhs Team' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Collude untill betrayal'
+strategy_description = 'Collude first round. untill betrayal'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    my_score, their_score are ints.'''
     
-    Make my move.
+    if len(my_history)==0: 
+        return 'c'
+    elif my_history[-1]=='c' and their_history[-1]=='b':
+        return 'b' 
+    else:
+        return 'c' 
+    
+    '''Make my move.
     Returns 'c' or 'b'. 
     '''
 
